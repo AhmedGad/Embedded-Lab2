306bb6793e3f727ad766b2326bd15859b5cbaf3b
Gad <Ahmed.Mu.Gad@gmail.com>
2013-02-23 00:51:57 +0200

    empty j2me project

---
+++ src/Lab1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.microedition.midlet.*;
+
+/**
+ * @author Ahmed Gad, Ahmed Ibrahem
+ */
+
+public class Lab1 extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+        
+    }
+}
